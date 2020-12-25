5ad126af3cb4d32cb1d4f03670cb2945436148e6
Dikshit Bhardwaj <54199681+iamdikshit@users.noreply.github.com>
2020-12-24 22:02:21 -0800

    Add files via upload

---
+++ python/python_assignment2.py
@@ -0,0 +1,5 @@
+
+name = input('\nEnter First Name:')
+last_name = input('\n Enter Last Name:')
+
+print(last_name[::-1]+' '+name[::-1])
---
+++ python/python_assignment3.py
@@ -0,0 +1,6 @@
+#Write a Python program to find the volume of a sphere with diameter 12 cm.
+
+diameter = 12
+radius = diameter/2
+volume = (4/3)*(3.14)*(radius)**3
+print(volume)
